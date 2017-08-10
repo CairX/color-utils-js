db00d2697402382d79a9c42e65644a2807d857a3
cairns <lazycairx@gmail.com>
2017-08-10 08:38:12 +0200

    Remove obsolete comments

--- colors.js
+++ colors.js
@@ -4,21 +4,9 @@
 "use strict";
 
 var Colors = (function() {
-
-	/* ------------------------------------------------- *
-	 * Exporeted methods.
-	 * ------------------------------------------------- */
 	var self = {};
 	self.rgb = {};
 
-
-	/* ------------------------------------------------- *
-	 * Round the RGB value. Limited to 0-255.
-	 *
-	 * @param number {Integer} The number to round.
-	 *
-	 * @return {Integer} Value from 0 to 255.
-	 * ------------------------------------------------- */
 	self.rgb.round = function(number) {
 		number = Math.round(number);
 		if (number < 0) {
@@ -30,18 +18,6 @@ var Colors = (function() {
 		}
 	};
 
-
-	/* ------------------------------------------------- *
-	 * Extract RGB values from string based on the
-	 * following format: rgb(xxx, xxx, xxx)
-	 *
-	 * @param string {String} Color string.
-	 *
-	 * @return {Array} Integer color values.
-	 *                 Red = 0
-	 *                 Green = 1
-	 *                 Blue = 2
-	 * ------------------------------------------------- */
 	self.rgb.extract = function(string) {
 		var pattern = /rgb\((\d{1,3}),\s?(\d{1,3}),\s?(\d{1,3})\)/;
 		var colors = pattern.exec(string.toLowerCase());
@@ -54,32 +30,10 @@ var Colors = (function() {
 		return colors;
 	};
 
-
-	/* ------------------------------------------------- *
-	 * Convert array of color values into string.
-	 *
-	 * @param colors {Array} List of colors with an
-	 *                       expected order of red(0),
-	 *                       green(1) and blue(2).
-	 *
-	 * @return {string} Colors in string format:
-	 *                  rgb(xxx, xxx, xxx)
-	 * ------------------------------------------------- */
 	self.rgb.string = function(colors) {
 		return "rgb(" + colors[0] + ", " + colors[1] + ", " + colors[2] + ")";
 	};
 
-
-	/* ------------------------------------------------- *
-	 * Create list of colors that transist from given
-	 * start value to end.
-	 *
-	 * @param start {String} Color, rgb(xxx, xxx, xxx).
-	 * @param end {String} Color, rgb(xxx, xxx, xxx).
-	 * @param steps {Integer} Number of transition steps.
-	 *
-	 * @return {Array} List of color strings.
-	 * ------------------------------------------------- */
 	self.rgb.transition = function(start, end, steps) {
 		// TODO: Take color list to create multiple transitions instead of start and end.
 		start = self.rgb.extract(start);
@@ -102,17 +56,6 @@ var Colors = (function() {
 		return colors;
 	};
 
-
-	/* ------------------------------------------------- *
-	 * Transition between multiple colors in the number
-	 * of given steps.
-	 *
-	 * @param colors {Array} List of colors as string
-	 *                       in format, rgb(xxx, xxx, xxx).
-	 * @param steps {Integer} Number of transition steps.
-	 *
-	 * @return {Array} List of color strings.
-	 * ------------------------------------------------- */
 	self.rgb.transitions = function(colors, steps) {
 		if (colors.length >= steps) {
 			return colors;
@@ -147,19 +90,6 @@ var Colors = (function() {
 		}
 	};
 
-
-	/* ------------------------------------------------- *
-	 * Create list of colors that transist from given
-	 * first and second color. Where if looped will
-	 * create a continuous transition from one color
-	 * to the other, much like a pulse.
-	 *
-	 * @param first {String} Color, rgb(xxx, xxx, xxx).
-	 * @param second {String} Color, rgb(xxx, xxx, xxx).
-	 * @param steps {Integer} Number of transition steps.
-	 *
-	 * @return {Array} List of color strings.
-	 * ------------------------------------------------- */
 	self.rgb.loop = function(first, second, steps) {
 		steps = (steps + 2) / 2;
 		var transition = self.rgb.transition(first, second, steps);
