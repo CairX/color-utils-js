84a2f39f55c2fbfdf971b4b43dd0563c86b81dbb
cairns <lazycairx@gmail.com>
2017-08-13 01:46:17 +0200

    Support the hex color format, accepts length 3, 4, 6 and 8

--- colors.js
+++ colors.js
@@ -7,6 +7,17 @@ var Colors = (function() {
 	var self = {};
 	var raw = {};
 
+	var splitInterval = function(string, interval) {
+		return string.toString().match(new RegExp(".{1," + interval + "}", "g"));
+	};
+
+	var padding = function(number) {
+		if (number.toString().length === 1) {
+			return "0" + number.toString();
+		}
+		return number;
+	};
+
 	var formats = [
 		{
 			id: "rgb",
@@ -29,6 +40,62 @@ var Colors = (function() {
 			},
 			string: function(raw) {
 				return "rgba(" + raw.red + ", " + raw.green + ", " + raw.blue + ", " + (raw.alpha / 255).toFixed(3) + ")";
+			}
+		}, {
+			id: "hex-36",
+			pattern: /^#([0-9a-f]{3}|[0-9a-f]{6})$/im,
+			weight: 0,
+			extract: function(string) {
+				var values = this.pattern.exec(string)[1];
+				if (values.length === 3) {
+					values = splitInterval(values, 1).map(function(value) {
+						return value + value;
+					});
+				} else if (values.length === 6) {
+					values = splitInterval(values, 2);
+				}
+				values = values.map(function(value) {
+					return parseInt(value, 16);
+				});
+				return { red: values[0], green: values[1], blue: values[2], alpha: 255 };
+			},
+			string: function(raw) {
+				Object.keys(raw).forEach(function(key) {
+					raw[key] = padding(raw[key].toString(16));
+				});
+				var value = "#" + raw.red + "" + raw.green + "" + raw.blue;
+				if (raw.alpha !== "ff") {
+					value += raw.alpha;
+				}
+				return value;
+			}
+		}, {
+			id: "hex-48",
+			pattern: /^#([0-9a-f]{4}|[0-9a-f]{8})$/im,
+			weight: 1,
+			extract: function(string) {
+				var values = this.pattern.exec(string)[1];
+				if (values.length === 4) {
+					values = splitInterval(values, 1).map(function(value) {
+						return value + value;
+					});
+				} else if (values.length === 8) {
+					values = splitInterval(values, 2);
+				}
+				values = values.map(function(value) {
+					return parseInt(value, 16);
+				});
+				return { red: values[0], green: values[1], blue: values[2], alpha: values[3] };
+			},
+			string: function(raw) {
+				Object.keys(raw).forEach(function(key) {
+					raw[key] = padding(raw[key].toString(16));
+				});
+				var value = "#" + raw.red + "" + raw.green + "" + raw.blue;
+				if (raw.alpha !== "ff") {
+					value += raw.alpha;
+				}
+				return value;
 			}
 		}
 	];
@@ -129,7 +196,6 @@ var Colors = (function() {
 			for (var k = 1; k < colors.length; k++) {
 				format = format.weight >= colors[k].format.weight ? format : colors[k].format;
 			}
-			console.log(format);
 
 			return result.map(function(color) {
 				return format.string(color);
